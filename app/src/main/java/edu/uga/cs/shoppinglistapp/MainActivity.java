2bc5b2b7134debe79d4a69bfad83cb5117dfc85b
Michael Beale <mtbeale@hotmail.com>
2018-11-25 12:39:59 -0500

    changed to listview
    will retrieve from firebase today

--- app/src/main/java/edu/uga/cs/shoppinglistapp/MainActivity.java
+++ app/src/main/java/edu/uga/cs/shoppinglistapp/MainActivity.java
@@ -8,14 +8,24 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private FirebaseDatabase mFirebaseDatabase;
@@ -30,10 +40,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FirebaseUtil.openFbReference("shoppinglists", this);
         mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
         mDatabaseReference = FirebaseUtil.mDatabaseReference;
-        mFirebasebtn = (Button) findViewById(R.id.save_button);
-        txtTitle = (EditText) findViewById(R.id.txtTitle);
 
-        mFirebasebtn.setOnClickListener(this);
+
+        /**
+         * Getting already made lists from the database...
+         */
+        final ArrayList<String> allLists = new ArrayList<String>();
+        mDatabaseReference.child("shoppinglists").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (int i = 0; i < 3; i++) {
+                    allLists.add(dataSnapshot.getValue(String.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        Button btn= (Button) findViewById(R.id.newList);
+
+        String [] lists = new String [] {
+                "List",
+                "Walmart"
+        };
+
+        final List<String> testList = new ArrayList<String>(Arrays.asList(lists));
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,testList);
+        listView.setAdapter(arrayAdapter);
+
+        txtTitle = (EditText) findViewById(R.id.listName);
+
+        btn.setOnClickListener(this);
     }
 
     @Override

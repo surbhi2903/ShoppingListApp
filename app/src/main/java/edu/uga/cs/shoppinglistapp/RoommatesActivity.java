8e40a1bc963dff6429e3290c82055135d32b34f6
surbhi2903 <p.surbhi2903@gmail.com>
2018-11-29 19:05:52 -0500

    displaying roommates Id

--- app/src/main/java/edu/uga/cs/shoppinglistapp/RoommatesActivity.java
+++ app/src/main/java/edu/uga/cs/shoppinglistapp/RoommatesActivity.java
@@ -1,14 +1,21 @@
 package edu.uga.cs.shoppinglistapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -29,21 +36,22 @@ public class RoommatesActivity extends AppCompatActivity implements View.OnClick
         setContentView(R.layout.activity_roommates);
 //        mFirebaseDatabase = FirebaseDatabase.getInstance();
 //        mDatabaseReference = mFirebaseDatabase.getReference().child("users");
-        emailId = (EditText)findViewById(R.id.roommate_text);
-        checkEmailId = (Button)findViewById(R.id.button);
+
         FirebaseUtil.openFbReference("users", this);
         mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
         mDatabaseReference = FirebaseUtil.mDatabaseReference;
-//        final ListView listView = (ListView) findViewById(R.id.roommate_list);
+        emailId = (EditText)findViewById(R.id.roommate_text);
+        checkEmailId = (Button)findViewById(R.id.button);
         auth = FirebaseAuth.getInstance();
-//
-//        final ListAdapter myAdapter = new FirebaseListAdapter<User>(this, User.class, android.R.layout.simple_list_item_2, mDatabaseReference) {
-//            @Override
-//            protected void populateView(View v, User user, int position) {
-//                ((TextView)v.findViewById(android.R.id.text2)).setText(user.getEmail());
-//            }
-//        };
-//        listView.setAdapter(myAdapter);
+
+        final ListAdapter myAdapter1 = new FirebaseListAdapter<User>(this, User.class, android.R.layout.simple_list_item_2, mDatabaseReference) {
+            @Override
+            protected void populateView(View v, User user, int position) {
+                ((TextView)v.findViewById(android.R.id.text2)).setText(user.getEmail());
+            }
+        };
+        final ListView listView = (ListView) findViewById(R.id.roommate_list);
+        listView.setAdapter(myAdapter1);
        checkEmailId.setOnClickListener(this);
     }
     @Override
@@ -88,6 +96,40 @@ public class RoommatesActivity extends AppCompatActivity implements View.OnClick
     private void clean() {
         emailId.setText("");
         emailId.requestFocus();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout_menu:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.d("Logout", "User Logged Out");
+                                FirebaseUtil.attachListener();
+                            }
+                        });
+                FirebaseUtil.detachListener();
+                return true;
+            case R.id.roommates:
+                Intent i = new Intent(this,RoommatesActivity.class);
+                this.startActivity(i);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FirebaseUtil.detachListener();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseUtil.attachListener();
     }
 
 

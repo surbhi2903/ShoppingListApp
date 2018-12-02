925658e5137913bc1c0cc4c15337dfab08d2df82
Michael Beale <mtbeale@hotmail.com>
2018-12-02 16:45:22 -0500

    Update NewItemActivity.java

--- app/src/main/java/edu/uga/cs/shoppinglistapp/NewItemActivity.java
+++ app/src/main/java/edu/uga/cs/shoppinglistapp/NewItemActivity.java
@@ -1,13 +1,29 @@
 package edu.uga.cs.shoppinglistapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,62 +33,42 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.Map;
 
-public class NewItemActivity extends AppCompatActivity  {
+public class ViewListActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public static final String DEBUG_TAG = "NewItemActivity";
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference mDatabaseReference;
-
-    private EditText itemName;
-    private EditText itemCost;
-    private EditText paidBy;
-    private Button saveButton;
-
-    String listName = null;
-    String listKey = null;
-    ArrayList<String> names;
-    ArrayList<String> listNames;
-    ArrayList<String> listKeys;
-
+    TextView listName;
+    String listKey;
+    String data;
+    int itemPosition;
+    ArrayList<GroceryItem> groceryItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_item);
-        itemName = (EditText) findViewById(R.id.item);
-        itemCost = (EditText) findViewById(R.id.item_cost);
-        paidBy = (EditText) findViewById(R.id.paidName);
-        saveButton = (Button) findViewById(R.id.button);
-
+        setContentView(R.layout.activity_view_list);
         Bundle extras = getIntent().getExtras();
-
-        listName = extras.getString("listName");
-        listKeys = new ArrayList<>();
-
+        data = extras.getString("listName");
+        listKey = extras.getString("id");
+        Log.e("listkey", listKey);
         FirebaseUtil.openFbReference("shoppinglists", this);
         mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
         mDatabaseReference = FirebaseUtil.mDatabaseReference;
-
-        names = new ArrayList<>();
+        listName = (TextView)findViewById(R.id.listName);
+        listName.setText(data);
+        final ListView listView = (ListView) findViewById(R.id.itemLV);
+        Button btn = (Button)findViewById(R.id.addItemBtn);
         mDatabaseReference.addListenerForSingleValueEvent(
                 new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                            collectListNames((Map<String, ShoppingList>) dataSnapshot.getValue());
-                            if (dataSnapshot.exists()) {
-                                int i = 0;
-                                for (DataSnapshot d: dataSnapshot.getChildren()) {
-                                    listKeys.add(d.getKey());
-                                    i++;
-                                    Log.e("Key",d.getKey());
-                                    Log.e("Title", d.child("title").toString());
-
-                                    if (d.child("title").getValue().toString().equals(listName)) {
-                                        listKey = d.getKey();
-                                    }
-                                }
-                            }
+                        groceryItems = new ArrayList<>();
+                        //collectListNames((Map<String, GroceryItem>) dataSnapshot.getValue());
+                        if (dataSnapshot.exists()) {
+                            int i = 0;
+                        }
+                        GroceryItem test = dataSnapshot.child(listKey).child("items").getValue(GroceryItem.class);
+                        groceryItems.add(test);
                     }
 
                     @Override
@@ -82,40 +78,91 @@ public class NewItemActivity extends AppCompatActivity  {
                 }
         );
 
-        saveButton.setOnClickListener(new ButtonClickListener());
+        listView.setLongClickable(true);
+         ListAdapter myAdapter = new FirebaseListAdapter<GroceryItem>(this, GroceryItem.class,
+                R.layout.item_list, mDatabaseReference.child(listKey).child("items")) {
+
+            @Override
+            protected void populateView(View v, GroceryItem item, int position) {
+                Log.e("items", item.getItemName());
+                ((TextView) v.findViewById(R.id.iName)).setText(item.getItemName());
+                ((TextView) v.findViewById(R.id.item_cost)).setText(item.getItemCost());
+                ((TextView) v.findViewById(R.id.purchasedBy)).setText(item.getPurchasedBy());
+
+            }
+        };
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(ViewListActivity.this);
+                dialogBuilder.setTitle("Delete grocery list?");
+                dialogBuilder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Pressed", Toast.LENGTH_LONG).show();
+
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "List not deleted."
+                                , Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialogBuilder.create().show();
+                return true;
+            }
+        });
+        listView.setAdapter(myAdapter);
+        registerForContextMenu(listView);
+        btn.setOnClickListener(this);
 
     }
 
-    private void collectListNames(Map<String,ShoppingList> lists) {
-        listNames = new ArrayList<>();
-
-        for (Map.Entry<String, ShoppingList> entry : lists.entrySet()) {
-            Map singleList = (Map) entry.getValue();
-
-            listNames.add(singleList.get("title").toString());
-
-        }
-        Log.e("list",listNames.toString());
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), NewItemActivity.class);
+        String name = listName.getText().toString();
+        intent.putExtra("listName",name);
+        intent.putExtra("id", listKey);
+        v.getContext().startActivity(intent);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.items_activity_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.roommates:
+                Intent roommate = new Intent(this,RoommateActivity.class);
+                roommate.putExtra("listName",data);
+                roommate.putExtra("id",listKey);
+                this.startActivity(roommate);
+                return true;
+            case R.id.shopping_list:
+                Intent i = new Intent(this,MainActivity.class);
+                this.startActivity(i);
+                return true;
 
-    private class ButtonClickListener implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-
-            String item = itemName.getText().toString();
-            String cost = itemCost.getText().toString();
-            String name = paidBy.getText().toString();
-
-            GroceryItem newItem = new GroceryItem(item, cost, name);
-          //  groupId = mDatabaseReference.push().getKey();
-            Log.e("list",listNames.toString());
-            //Log.e("key", listKey.toString());
-            mDatabaseReference.child(listKey).child("items").push().setValue(newItem);
-            itemName.setText("");
-            itemCost.setText("");
-            paidBy.setText("");
         }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FirebaseUtil.detachListener();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseUtil.attachListener();
     }
 }
